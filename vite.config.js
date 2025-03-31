9b5bd34f9f07f7c22e7a5a33c130df72e5af7d1d
Adrianegori <adriane.leberiaga06@gmail.com>
2025-03-31 16:43:45 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,5 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
 export default defineConfig({
-  plugins: [react()],
   build: {
-    outDir: "build", // Change to "build" or your preferred folder name
+    outDir: "dist", // Default Vite setting
   },
 });
